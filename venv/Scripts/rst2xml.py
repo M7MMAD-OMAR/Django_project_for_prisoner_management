23ef8a7b4b7ccd4d352b50519c3414883e0c99f4
m7mmad <muhammadaaa727@gmial.com>
2022-05-27 17:34:32 +0300

    Add all file

---
+++ venv/Scripts/rst2xml.py
@@ -0,0 +1,23 @@
+#!C:\Users\muham\Desktop\Pr_hw_MUHAMMAD_OMAR_HAJ_HAMDO\Mobile_app_for_prisoner_management\venv\Scripts\python.exe
+
+# $Id: rst2xml.py 4564 2006-05-21 20:44:42Z wiemann $
+# Author: David Goodger <goodger@python.org>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A minimal front end to the Docutils Publisher, producing Docutils XML.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+
+
+description = ('Generates Docutils-native XML from standalone '
+               'reStructuredText sources.  ' + default_description)
+
+publish_cmdline(writer_name='xml', description=description)
