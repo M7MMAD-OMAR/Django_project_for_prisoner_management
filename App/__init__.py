798ba2271d0b19395299f9847d8db20dec56afd4
m7mmad <muhammadaaa727@gmial.com>
2022-05-27 17:50:54 +0300

    test 2

--- App/__init__.py
+++ App/__init__.py
@@ -1,10 +1,3 @@
-from kivy.app import App
-from kivy.uix.button import Button
-
-
-class TestApp(App):
-    def build(self):
-        return Button(text='Hello World')
-
-
-TestApp().run()
+class app():
+    pass
+print("fuck you bitch")
