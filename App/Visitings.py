80a8b0bc38d8743e1880b539e46bfd7c7e05b449
m7mmad <muhammadaaa727@gmial.com>
2022-06-03 23:27:33 +0300

    Create many methods(select_person_inside_dungeons, select_persons_by_offense, select_persons_between_date, select_visitor_by_dateTime

--- App/Visitings.py
+++ App/Visitings.py
@@ -21,14 +21,13 @@ class Visitings:
             temp_str = """SELECT * FROM Visitings"""
             count = 0
             for row in db.cursor().execute(temp_str).fetchall():
-                count+=1
+                count += 1
                 print(str(count), "".center(50, '-'))
                 print(f'ID:              {row[0]}\n'
                       f'Date Visited:    {row[1]}\n'
                       f'Person Id:       {row[2]}\n'
                       f'Visitor Name:    {row[3]}\n'
                       f'Mountin Minutes: {row[4]}')
-                print(type(row[4]))
         except Exception as ex:
             raise Exception(ex)
         finally:
@@ -37,9 +36,9 @@ class Visitings:
 
     @classmethod
     def add_Visiting(cls, date_visited: c_DB.d, person_id: int, visitor_name: str, mountIn_minutes):
+        """Add visitor in DB and check values"""
         global db
         try:
-            # db = c_DB.connect_DB()
             v = Visitings(date_visited, person_id, visitor_name, mountIn_minutes)
             db = c_DB.connect_DB()
             temp_str = """INSERT INTO Visitings('date_visited', 'person_id', 'visitor_name', 'mountin_minuts')
@@ -47,6 +46,42 @@ class Visitings:
             temp_val = (v.date_visited, v.person_id, v.visitor_name, v.mountIn_minutes.strftime("%H:%M"))
             db.cursor().execute(temp_str, temp_val)
             db.commit()
+        except Exception as ex:
+            raise Exception(ex)
+        finally:
+            if db:
+                db.close()
+
+    @classmethod
+    def select_visitor_by_dateTime(cls, first_date, last_date, first_time=c_DB.t(00, 00), last_time=c_DB.t(23, 59)):
+        """Results all values if date and time inside range"""
+        global db
+        try:
+            db = c_DB.connect_DB()
+            if first_date > last_date:
+                first_date, last_date = last_date, first_date
+            temp_str = """SELECT * FROM Visitings WHERE (date_visited BETWEEN :fd AND :ld)
+                                                    AND (mountin_minuts BETWEEN :ft AND :lt)"""
+            temp_val = (
+                {"fd": first_date, "ld": last_date, "ft": first_time.strftime("%H:%M"),
+                 "lt": last_time.strftime("%H:%M")})
+            # Format result
+            count = 0
+            print("#".center(7, ' '), end=' | ')
+            print("ID".center(7, ' '), end=' | ')
+            print("Date Visited".center(14, ' '), end=' | ')
+            print("Person ID".center(5, ' '), end=' | ')
+            print("Visitor Name".center(50, ' '), end=' | ')
+            print("Mount in Minutes".center(7, ' '), end=' | \n')
+            print("-" * 130)
+            for row in db.cursor().execute(temp_str, temp_val).fetchall():
+                count += 1
+                print(f' {count} '.center(7, ' '),
+                      f' {row[0]} '.center(12, ' '),
+                      f' {row[1]} '.center(10, ' '),
+                      f' {row[2]} '.center(16, ' '),
+                      f' {row[3]} '.center(50, ' '),
+                      f' {row[4]} '.center(20, ' '))
         except Exception as ex:
             raise Exception(ex)
         finally:
@@ -120,7 +155,6 @@ class Visitings:
     @mountIn_minutes.setter
     def mountIn_minutes(self, mm):
         try:
-
             self.__mountIn_minutes = mm
         except Exception as ex:
             raise Exception(ex.__class__)
--- App/main.py
+++ App/main.py
@@ -4,10 +4,14 @@ import Visitings as v
 
 """SELECT * FROM Visitings WHERE mountin_minuts BETWEEN '06:00' and '21:00'"""
 
+import Convicts as c
+import Dungeon as d, Person as p, Visitings as v, Offense as o, Dungeon_Moves as dm
+
 # import Convicts as c
 # import Dungeon as d, Person as p, Visitings as v, Offense as o, Dungeon_Moves as dm
 if __name__ == '__main__':
     try:
+
         # p.Person.add_person(' ali  ', ' omar  ', ' saleem  ', "  male", date(1199, 1, 1), 'Aziz')
         # p.Person.add_person('mona  ', ' omar', 'khaled  ', "female", date(2001, 11, 8), 'Turkey, Istanbul')
         # p.Person.add_person('   amane   ', '  muhammad  ', ' hamdo ', "     female", date(1199, 1, 1), 'istanbul')
@@ -19,17 +23,34 @@ if __name__ == '__main__':
         # c.Convicts.add_convicts(date(2005, 6, 8), date(2020, 1, 1), 116, 5)
         # c.Convicts.add_convicts(date(2019, 7, 5), date(2070, 1, 1), 117, 4)
         """Convicts(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)"""
-        # x = time(3, 40).strftime("%H:%M")
-        v.Visitings.add_Visiting(date(1199, 1, 1), 115, "aliiiii", time(3, 40))
-        # v.Visitings.__str__()
-        v.Visitings.add_Visiting(date(2001, 11, 8), 116, "amera", time(5, 50))
-        v.Visitings.add_Visiting(date(2011, 11, 8), 115, "fatema", time(6, 00))
-        v.Visitings.add_Visiting(date(2021, 10, 1), 117, "osama", time(20, 20))
+
+        # v.Visitings.add_Visiting(date(1199, 1, 1), 115, "aliiiii", time(3, 40))
+        # v.Visitings.add_Visiting(date(2001, 11, 8), 116, "amera", time(5, 50))
+        # v.Visitings.add_Visiting(date(2011, 11, 8), 115, "fatema", time(6, 00))
+        # v.Visitings.add_Visiting(date(2021, 10, 1), 117, "osama", time(20, 20))
+        """Visitings(23, 24, 25, 26"""
 
         # o.Offense.add_offense("sareka")
         # o.Offense.add_offense("test")
         """Offense(4, 5)"""
-        # c.Convicts.select_persons_between_date(date(2003, 10, 20), date(2004, 1, 1) )
+
+        # d.Dungeon.add_dungeon("zenzana 1   ", 10)
+        # d.Dungeon.add_dungeon("zenzana 2   ", 1)
+        # d.Dungeon.add_dungeon("zenzana 3   ", 5)
+        """Dungeon(10, 11, 12)"""
+
+        # dm.Dungeon_Moves.add_dungeon_moves(10, 115, date(2000, 10, 10))
+        # dm.Dungeon_Moves.add_dungeon_moves(10, 116, date(2002, 10, 10))
+        # dm.Dungeon_Moves.add_dungeon_moves(10, 116, date(2011, 10, 10))
+        # dm.Dungeon_Moves.add_dungeon_moves(10, 115, date(2006, 10, 10))
+        """Dungeon_Moves(6, 7, 8, 9, 10)"""
+        # dm.Dungeon_Moves.select_person_inside_dungeons(116)
+        # c.Convicts.select_persons_by_offense(4)
+        # c.Convicts.select_persons_between_date(date(2020, 10, 19), date(2005, 6, 7))
+        #
+        # v.Visitings.select_visitor_by_dateTime(date(1199, 1, 1), date(2011, 11, 8)
+        #                                             , time(3, 50), time(5, 51))
+        # v.Visitings.select_visitor_by_dateTime(date(1199, 1, 1), date(2011, 11, 8))
     except c_DB.sq.ProgrammingError as ex:
         print(ex)
     except TypeError as ex:
