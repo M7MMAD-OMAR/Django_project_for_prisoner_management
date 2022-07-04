2b5e8fd969048e9494f33a5d13eb9678fa03e389
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-07-04 18:43:33 +0300

    Added person.json and change method add_person And convert global db, To db = None

---
+++ App/test.py
@@ -0,0 +1,23 @@
+import json
+
+json_file = "../JSON/person.json"
+
+
+def write_json(date, json_file=json_file):
+    with open(json_file, "w") as jf:
+        json.dump(date, jf, indent=4)
+
+
+
+
+
+
+def add_person_with_json_file(id, name, age, father):
+    with open(json_file) as jf:
+        date = json.load(jf)
+        temp = date["persons"]
+        temp.append({"id": id, "name": name, "age": age, "father": father})
+        write_json(date)
+
+
+add_person_with_json_file(4, "fatema", 88, "omar")
